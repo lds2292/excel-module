2d77391c24cf45d4fe157a96b297b5e6ea5a25eb
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2023-02-15 21:24:50 +0900

    FileChecker 진행중

--- src/main/java/sfn/excel/module/kenya/FileChecker.java
+++ src/main/java/sfn/excel/module/kenya/FileChecker.java
@@ -2,25 +2,22 @@ package sfn.excel.module.kenya;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
 public class FileChecker {
 
     private final int EXCEL_SIGNATURE_SIZE = 4;
     private final int EXCEL_PASSWORD_SIGNATURE_SIZE = 4;
     private final byte[] EXCEL_FILE_SIGNATURE = {0x50, 0x4b, 0x03, 0x04};
+    private final byte[] EXCEL_PASSWORD_FILE_SIGNATURE = {
+        (byte) 0xD0, (byte) 0xCF, (byte) 0x11, (byte) 0xE0,
+        (byte) 0xA1, (byte) 0xB1, (byte) 0x1A, (byte) 0xE1
+    };
 
 
     // TODO : 여기서부터 시작
     public boolean isExcelFile(InputStream fileInputStream) throws IOException {
         byte[] bytes = fileInputStream.readNBytes(EXCEL_SIGNATURE_SIZE);
-        System.out.println(Arrays.toString(bytes));
 
-        byte[] bytes2 = fileInputStream.readNBytes(EXCEL_SIGNATURE_SIZE);
-        System.out.println(Arrays.toString(bytes2));
-
-        return true;
-//        return Arrays.equals(EXCEL_FILE_SIGNATURE, bytes);
     }
 
 }
